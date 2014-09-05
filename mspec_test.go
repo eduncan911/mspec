2891157aa84d7284ee4b2652a542801f150abd33
Eric Duncan <me@eduncan911.com>
2014-09-05 00:16:54 -0400

    fixing up mspec tests and readding

---
+++ mspec_test.go
@@ -0,0 +1,45 @@
+package gomspec
+
+import (
+	"testing"
+)
+
+func Test_Mspec_Instances(t *testing.T) {
+
+	Given(t, "an mspec instance", func(when When) {
+
+		f := "feature value"
+
+		mspecTest := &MSpecConfig{
+			lastFeature: f,
+			lastGiven:   "context value",
+			lastWhen:    "when value",
+			lastSpec:    "title value",
+		}
+
+		when("calling reset()", func(it It) {
+
+			mspecTest.resetLasts()
+
+			it("should not reset lastFeature as that is used globally", func(assert Assert) {
+				assert.NotEmpty(mspecTest.lastFeature)
+			})
+
+			it("should keep the lastFeature value", func(assert Assert) {
+				assert.Equal(mspecTest.lastFeature, f)
+			})
+
+			it("should set lastGiven to zero value", func(assert Assert) {
+				assert.Empty(mspecTest.lastGiven)
+			})
+
+			it("should set lastWhen to zero value", func(assert Assert) {
+				assert.Empty(mspecTest.lastWhen)
+			})
+
+			it("should set lastSpec", func(assert Assert) {
+				assert.Empty(mspecTest.lastSpec)
+			})
+		})
+	})
+}
